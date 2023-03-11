b5964991adceba64f69c6386c642190e4c3dd41f
amin-alavian <112092631+m-amin-alavian@users.noreply.github.com>
2023-03-11 17:43:10 +0330

    Edit inputs

--- econdash/pages/1_solow-swan_model.py
+++ econdash/pages/1_solow-swan_model.py
@@ -12,7 +12,9 @@ col1, col2 = st.columns([1, 2], gap="medium")
 
 with col1:
     st.write("$$ Y(t) = K(t)^\\alpha(A(t)L(t))^{1-\\alpha} $$")
-    depreciation_rate = st.slider(label='$ "\\delta" $', min_value=0.0, max_value=1.0, value=0.2, step=0.01)
+    lat = st.slider('$ A $, Labor-Augmenting Technology', min_value=1.0, max_value=5.0, value=1.2, step=0.1)
+    alpha = st.slider(label="$ \\alpha $", min_value=0.0, max_value=1.0, value=0.3, step=0.01)
+    depreciation_rate = st.slider(label='$ \\delta $', min_value=0.0, max_value=1.0, value=0.2, step=0.01)
 
 with col2:
     uniform = np.arange(0, 10, 0.05)
